7efc296815e25f13e1cf3913102c3dd5a73a5321
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-06-26 13:28:20 +0430

    added pages to project

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from telnetlib import TELNET_PORT
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutUsView(TemplateView):
+    template_name = "pages/aboutus.html"
