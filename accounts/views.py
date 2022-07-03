8f12236b8aa2c08ef164da83ee70d40d0d0e1da9
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-07-03 14:06:27 +0430

    Add registration and crispy forms to project

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from re import T
+from sre_constants import SUCCESS
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('home')
