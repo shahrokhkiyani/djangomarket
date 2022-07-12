8456401205bb47d3b7e8d19b9242718e4e718641
Shahrokh Kiyani <shahrokhkiyani@protonmail.com>
2022-07-11 23:44:54 -0700

    Added AllAuth to project

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,3 @@
-from re import T
-from sre_constants import SUCCESS
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-from .forms import CustomUserCreationForm
-
-
-class SignUpView(CreateView):
-
-    form_class = CustomUserCreationForm
-    template_name = 'registration/signup.html'
-    success_url = reverse_lazy('home')
