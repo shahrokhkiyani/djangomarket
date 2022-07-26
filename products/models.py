66c5b066bfdf0e8e7e9e6d6b4dfb681d4d8652f6
= <=>
2022-07-26 10:25:10 +0430

    added product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    ACTIVE_CHOICES = (
+        ('avb', 'Available'),
+        ('una', 'Unavailable')
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    active = models.CharField(max_length=3, choices=ACTIVE_CHOICES)
+    # cover = models.ImageField()
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
