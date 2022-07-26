1adb40c9f7cd87bd3bbfe4a7edf85b3fe89f6f23
= <=>
2022-07-26 10:46:01 +0430

    added env to project

--- products/views.py
+++ products/views.py
@@ -4,10 +4,6 @@ from .models import Product
 
 
 class ProductListView(generic.ListView):
-    model = Product
+    queryset = Product.objects.filter(active=True)
     template_name = 'products/productlist.html'
     context_object_name = 'products'
-
-
-    def __str__(self):
-        return self.title
